fa15f8ee233ff37fca7eb9d8c15779f715e4ac8e
Nuhash Chowdhury <nuhashchowdhury@outlook.com>
2022-10-24 12:49:40 -0400

    Add form and styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,10 +10,18 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="">
-        <h1 className="text-3xl text-cyan-800">
+      <main className="max-w-screen-md mx-auto">
+        <h1 className="text-4xl text-center font-serif font-bold p-2 my-8">
           Promises
         </h1>
+
+        <div className="w-full text-xl text-center p-8 bg-white drop-shadow mt-4 h-['80vh']">
+          <form className="leading-10 flex flex-col gap-2 items-center">
+            <span>User promises <input className="outline-none text-center border-b-2 border-b-yellow-200" placeholder="search user" /> that</span>
+            <input className="outline-none text-center w-4/5 border-b-2 border-b-yellow-200 mb-4" type="text" placeholder="what's the promise?" />
+            <br /><button className="border border-green-200 p-2 bg-green-100 mt-4 text-base">Create a new promise</button>
+          </form>
+        </div>
 
       </main>
     </div>
